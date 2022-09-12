5623644d07896c09c0734c08606804f7488bcf4e
Juan Diego Muñoz Ospina <jdmunozo_1fuqvirtual.edu.co>
2022-09-12 13:45:44 -0500

    Creado froNTEND veterinario

---
+++ MascotaFeliz.App/MascotaFeliz.App.Frontend/Pages/Veterinario/ListaVeterinario.cshtml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
+
+
+namespace MascotaFeliz.App.Frontend.Pages
+{
+    public class ListaVeterinarioModel : PageModel
+    {
+        private static IRepositorioVeterinario _repoVeterinario = new RepositorioVeterinario(new Persistencia.AppContext());
+
+        public IEnumerable<Veterinario> listaVeterinario { get;  set; }
+
+        public void OnGet()
+        {
+            listaVeterinario=_repoVeterinario.GetAllVeterinarios();
+        }
+    }
+}
